398032c7f63e0b337e3221307be4dfb4f4cd9c0f
jed jarin <jedjarin@gmail.com>
2026-03-09 22:27:04 +0900

    manipulation

---
+++ f_manipulating_strings.c/a_string_manipulation.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    unsigned int length;
+    char buf[80];
+
+    while (1) {
+
+        printf("\nEnter a line of text (blank line to stop): ");
+        fgets(buf, sizeof(buf), stdin);
+
+        length = strlen(buf);
+
+        if (length <= 1)
+            break;
+
+        printf("That line is %u characters long.\n", length - 1);
+    }
+
+    return 0;
+}
