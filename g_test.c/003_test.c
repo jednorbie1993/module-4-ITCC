1060a888e631211ca080fc8b26b274a5b5f489a8
jed jarin <jedjarin@gmail.com>
2026-03-11 23:08:26 +0900

    test

---
+++ g_test.c/003_test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+struct employee {
+    char name[50];
+    char child1[50];
+    char child2[50];
+};
+
+int main() {
+
+    printf("Question:\n");
+    printf("Write the structure to store the name of an employee and her two children.\n\n");
+
+    printf("Structure defined successfully.\n");
+
+    return 0;
+}
+/*
+Employee with Two Children Structure
+
+File: employee_children.c
+*/
